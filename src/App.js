990307b5ef8e19ae99047f6ba9547e4c1a40c941
sahil-au7 <sahilyadav601@gmail.com>
2021-01-18 00:11:13 +0530

    initial setup is done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utilities/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBdebPJK0O-cSNnAQlgW3Wj_8bY92M8CdE",
+  authDomain: "clone-f786b.firebaseapp.com",
+  projectId: "clone-f786b",
+  storageBucket: "clone-f786b.appspot.com",
+  messagingSenderId: "481543322530",
+  appId: "1:481543322530:web:75f7e48bcd312f23e990fb",
+  measurementId: "G-ZHV7MZ5BRZ",
+};
